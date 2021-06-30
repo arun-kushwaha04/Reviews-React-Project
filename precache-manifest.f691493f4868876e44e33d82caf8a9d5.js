f567a7623df550ba5073941a8a8d722209b02da0
arun-kushwaha04 <arunkushwaha89765@gmail.com>
2021-06-30 11:01:16 +0530

    Updates

---
+++ precache-manifest.f691493f4868876e44e33d82caf8a9d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be2fb148098cf2dc27545b3f21f3c42e",
+    "url": "/arun-kushwaha04/Reviews-React-Project.git/index.html"
+  },
+  {
+    "revision": "a37641126669c2de90e2",
+    "url": "/arun-kushwaha04/Reviews-React-Project.git/static/css/main.eb44a210.chunk.css"
+  },
+  {
+    "revision": "e1a3c50a15b44825207f",
+    "url": "/arun-kushwaha04/Reviews-React-Project.git/static/js/2.bdbc782b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/arun-kushwaha04/Reviews-React-Project.git/static/js/2.bdbc782b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a37641126669c2de90e2",
+    "url": "/arun-kushwaha04/Reviews-React-Project.git/static/js/main.65936661.chunk.js"
+  },
+  {
+    "revision": "27452f819b78d2c7b40c",
+    "url": "/arun-kushwaha04/Reviews-React-Project.git/static/js/runtime-main.48963080.js"
+  }
+]);
